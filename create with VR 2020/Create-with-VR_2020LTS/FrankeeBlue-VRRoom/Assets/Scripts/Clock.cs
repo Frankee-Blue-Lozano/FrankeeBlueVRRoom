f214dce93ec48eff7fea706d94dc711a4c31c240
Frankee-Blue-Lozano <91489835+Frankee-Blue-Lozano@users.noreply.github.com>
2022-10-04 15:11:33 -0700

    adding stuff to the room

--- create with VR 2020/Create-with-VR_2020LTS/FrankeeBlue-VRRoom/Assets/Scripts/Clock.cs
+++ create with VR 2020/Create-with-VR_2020LTS/FrankeeBlue-VRRoom/Assets/Scripts/Clock.cs
@@ -8,7 +8,7 @@ public class Clock : MonoBehaviour
 {
     public GameObject secondHand;
     public GameObject minuteHand;
-    public GameObject hourHands;
+    public GameObject hourHand;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +20,14 @@ public class Clock : MonoBehaviour
     void Update()
     {
         DateTime nowTime = DateTime.Now;
+        
         float secondDegree = (nowTime.Second / 60f) *  360f;
         secondHand.transform.localRotation = Quaternion.Euler(new Vector3(secondDegree, 0, 0));
+
+        float minuteDegree = (nowTime.Minute / 60f) * 360f;
+        minuteHand.transform.localRotation = Quaternion.Euler(new Vector3(minuteDegree, 0, 0));
+
+        float hourDegree = (nowTime.Hour / 12f) * 360f;
+        hourHand.transform.localRotation = Quaternion.Euler(new Vector3(hourDegree, 0, 0));
     }
 }
